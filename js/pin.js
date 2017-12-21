dd172fd4d3c0f290aafda8bf266ca1a1b1e8d7df
Vlad <programmist.vke@yandex.ru>
2017-12-22 00:18:09 +0300

    Личный проект: максимум подвижности

--- js/pin.js
+++ js/pin.js
@@ -43,12 +43,9 @@
       marker.style.top = offer.location.y - 40 + 'px';
       marker.className = 'map__pin';
       marker.innerHTML = '<img src="' + offer.author.avatar + '" width="40" height="40" draggable="false">';
-
       pinBind(marker, offer);
-
       fragment.appendChild(marker);
     }
-
     mapPins.appendChild(fragment);
   }
 
@@ -57,4 +54,63 @@
     mapPinActive: mapPinActive
   };
 
+  var mainPin = document.querySelector('.map__pin--main');
+  mainPin.addEventListener('mousedown', function (e) {
+    e.preventDefault();
+
+    var startCoords = {
+      x: e.clientX,
+      y: e.clientY
+    };
+    var shift = {};
+    var markerCoords = {};
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      markerCoords = {
+        x: mainPin.offsetLeft,
+        y: mainPin.offsetTop
+      };
+
+      mainPin.style.top = (mainPin.offsetTop - shift.y) + 'px';
+      mainPin.style.left = (mainPin.offsetLeft - shift.x) + 'px';
+
+      if (markerCoords.x < 300) {
+        mainPin.style.left = 300 + 'px';
+      }
+      if (markerCoords.x > 900) {
+        mainPin.style.left = 900 + 'px';
+      }
+
+      if (markerCoords.y < 100) {
+        mainPin.style.top = 100 + 'px';
+      }
+      if (markerCoords.y > 500) {
+        mainPin.style.top = 500 + 'px';
+      }
+
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+      document.getElementById('address').value = parseInt(mainPin.style.left, 10) + ', ' + parseInt(mainPin.style.top, 10);
+      window.map.mapInit();
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
 })();
